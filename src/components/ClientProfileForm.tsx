c396b7841c4772266c508d544b0a6b25ab83f398
Robbie Turner <robbieturner@mac.myfiosgateway.com>
2026-02-17 11:12:33 -0500

    Restore ClientProfileForm to unblock build

---
+++ src/components/ClientProfileForm.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function ClientProfileForm() {
+  return (
+    <div>
+      <h2>Client Profile Form</h2>
+      <p>Placeholder component.</p>
+    </div>
+  );
+}
